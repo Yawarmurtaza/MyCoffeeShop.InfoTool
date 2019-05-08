78e5e1e84bfb96efa50c6c807a22eb7fe66d4ec2
Yawar <yawarmurtaza@gmail.com>
2019-05-08 21:24:49 +0100

    added coffee rating

---
+++ MyCoffeeShop.InfoTool.Model/CoffeeShopRating.cs
@@ -0,0 +1,11 @@
+﻿namespace MyCoffeeShop.InfoTool.Model
+{
+    public enum CoffeeShopRating
+    {
+        Excellent = 5,
+        VeryGood = 4,
+        Good = 3,
+        Satisfacotry = 2,
+        Poor = 1
+    }
+}
