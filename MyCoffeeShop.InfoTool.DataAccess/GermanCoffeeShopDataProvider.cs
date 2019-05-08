e3b8830e5ea187bc20f8371586a25482021f9cd4
Julia <Julia@gmail.com>
2019-05-08 23:57:53 +0100

    commiting german coffee shop

---
+++ MyCoffeeShop.InfoTool.DataAccess/GermanCoffeeShopDataProvider.cs
@@ -0,0 +1,15 @@
+﻿using MyCoffeeShop.InfoTool.Model;
+using System.Collections.Generic;
+
+namespace MyCoffeeShop.InfoTool.DataAccess
+{
+    public class GermanCoffeeShopDataProvider : ICoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> GetCoffeeShops()
+        {
+            yield return new CoffeeShop { BeansStockKg = 100, Location = "Barlin", PaperCupsInStock = 89, Rating = CoffeeShopRating.Satisfacotry };
+            yield return new CoffeeShop { BeansStockKg = 500, Location = "Hamburg", PaperCupsInStock = 99, Rating = CoffeeShopRating.VeryGood };
+            yield return new CoffeeShop { BeansStockKg = 200, Location = "Frankfurt", PaperCupsInStock = 55, Rating = CoffeeShopRating.Good };
+        }
+    }
+}
