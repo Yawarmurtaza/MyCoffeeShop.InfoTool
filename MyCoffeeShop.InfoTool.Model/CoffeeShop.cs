9da3737407c5512d85d4896afc43f0e3e71c8225
Yawar <yawarmurtaza@gmail.com>
2019-05-08 01:46:50 +0100

    added data access and model projects to the solution.

---
+++ MyCoffeeShop.InfoTool.Model/CoffeeShop.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MyCoffeeShop.InfoTool.Model
+{
+    public class CoffeeShop
+    {
+        public string Location { get; set; }
+        public int BeansStockKg { get; set; }
+    }
+}
