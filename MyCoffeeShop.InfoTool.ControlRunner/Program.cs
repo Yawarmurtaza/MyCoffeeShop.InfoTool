109f53aef0dda9d9e7395543621c0d03a68444bd
Yawar <yawarmurtaza@gmail.com>
2019-05-08 01:47:15 +0100

    Implement help command.

--- MyCoffeeShop.InfoTool.ControlRunner/Program.cs
+++ MyCoffeeShop.InfoTool.ControlRunner/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MyCoffeeShop.InfoTool.DataAccess;
+using MyCoffeeShop.InfoTool.Model;
+using System;
+using System.Collections.Generic;
 
 namespace MyCoffeeShop.InfoTool.ControlRunner
 {
@@ -7,6 +10,23 @@ namespace MyCoffeeShop.InfoTool.ControlRunner
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to My Coffee Shop Info Tool!");
+            Console.WriteLine("Type help to list available commands");
+            ICoffeeShopDataProvider provider = new DefaultCoffeeShopDataProvider();
+
+            while (true)
+            {
+                string inputLine = Console.ReadLine();
+                IEnumerable<CoffeeShop> coffeeShopData = provider.GetCoffeeShops();
+
+                if (string.Equals("help", inputLine, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("> Available coffee shop commands");
+                    foreach (CoffeeShop nextShop in coffeeShopData)
+                    {
+                        Console.WriteLine($"> {nextShop.Location}");
+                    }
+                }
+            }
         }
     }
 }
