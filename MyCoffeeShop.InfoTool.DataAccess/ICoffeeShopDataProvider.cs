0c3335be91cddc675baeb6fd1903f0669b8179c2
Yawar <yawarmurtaza@gmail.com>
2019-05-08 03:18:11 +0100

    added the interface and method headers  - Yawar

--- MyCoffeeShop.InfoTool.DataAccess/ICoffeeShopDataProvider.cs
+++ MyCoffeeShop.InfoTool.DataAccess/ICoffeeShopDataProvider.cs
@@ -4,8 +4,15 @@ using System.Collections.Generic;
 
 namespace MyCoffeeShop.InfoTool.DataAccess
 {
+    /// <summary>
+    /// Provides the coffee shop's data.
+    /// </summary>
     public interface ICoffeeShopDataProvider
     {
+        /// <summary>
+        /// Gets the coffee shops.
+        /// </summary>
+        /// <returns>Coffee shop collections.</returns>
         IEnumerable<CoffeeShop> GetCoffeeShops();
     }
 
