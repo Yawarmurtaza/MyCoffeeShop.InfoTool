230322241dc81c352871b7f6f427d532b7813fec
Julia <Julia@gmail.com>
2019-05-08 21:07:27 +0100

    added comments on the property and class header.

--- MyCoffeeShop.InfoTool.Model/CoffeeShop.cs
+++ MyCoffeeShop.InfoTool.Model/CoffeeShop.cs
@@ -2,10 +2,24 @@
 
 namespace MyCoffeeShop.InfoTool.Model
 {
+    /// <summary>
+    /// Defines the coffee shop.
+    /// </summary>
     public class CoffeeShop
     {
+        /// <summary>
+        /// Gets or sets the location of the coffee shop.
+        /// </summary>
         public string Location { get; set; }
+
+        /// <summary>
+        /// Gets or set the coffee beans in stock in KG.
+        /// </summary>
         public int BeansStockKg { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of paper cups in stock.
+        /// </summary>
         public int PaperCupsInStock { get; set; }
     }
 }
