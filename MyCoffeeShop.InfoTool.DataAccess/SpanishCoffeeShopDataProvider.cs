1d5f9fd847380aaa4c21234011127721dbabf9ca
Julia <Julia@gmail.com>
2019-05-08 23:38:09 +0100

    added spanish coffee

---
+++ MyCoffeeShop.InfoTool.DataAccess/SpanishCoffeeShopDataProvider.cs
@@ -0,0 +1,14 @@
+﻿using MyCoffeeShop.InfoTool.Model;
+using System.Collections.Generic;
+
+namespace MyCoffeeShop.InfoTool.DataAccess
+{
+    public class SpanishCoffeeShopDataProvider : ICoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> GetCoffeeShops()
+        {
+            yield return new CoffeeShop { BeansStockKg = 1000, Location = "Madrid", PaperCupsInStock = 50 };
+            yield return new CoffeeShop { BeansStockKg = 2000, Location = "Barcelona", PaperCupsInStock = 95 };            
+        }
+    }
+}
