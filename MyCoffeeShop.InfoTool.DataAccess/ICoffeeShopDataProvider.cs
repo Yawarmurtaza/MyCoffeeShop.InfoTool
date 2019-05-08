b5c44b8f39a5d473d27e1651d322fc18088af928
Yawar <yawarmurtaza@gmail.com>
2019-05-08 03:16:24 +0100

    Extracted the class in its own file, added uk provider.

--- MyCoffeeShop.InfoTool.DataAccess/ICoffeeShopDataProvider.cs
+++ MyCoffeeShop.InfoTool.DataAccess/ICoffeeShopDataProvider.cs
@@ -8,14 +8,4 @@ namespace MyCoffeeShop.InfoTool.DataAccess
     {
         IEnumerable<CoffeeShop> GetCoffeeShops();
     }
-
-    public class DefaultCoffeeShopDataProvider : ICoffeeShopDataProvider
-    {
-        public IEnumerable<CoffeeShop> GetCoffeeShops()
-        {
-            yield return new CoffeeShop { BeansStockKg = 100, Location = "Karachi" };
-            yield return new CoffeeShop { BeansStockKg = 500, Location = "Thatta" };
-            yield return new CoffeeShop { BeansStockKg = 200, Location = "Hyderabad" };
-        }
-    }
 }
